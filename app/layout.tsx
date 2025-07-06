eb35b355a64ceb713e84ac0ef5b67ff8b79ed8de
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-07-07 02:14:06 +0500

    fix: metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light" suppressHydrationWarning>
+      <head />
       <body
         className={`${geist.className} antialiased min-h-screen bg-bgColor flex flex-col`}
       >
