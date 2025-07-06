03d77a79e63972ac8d578b054422a35906b61a35
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-07-07 01:47:06 +0500

    fix: metadata added

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,13 @@ import { ProjectsSection } from "./components/sections/projects";
 import { SkillsSection } from "./components/sections/skills";
 import ContactSection from "./components/sections/contact";
 
-// Enable static generation
 export const dynamic = "force-static";
 export const revalidate = false;
 
-// SEO metadata
 export const metadata = {
   title: "Asif Zulfiqar - Software Engineer 👨🏻‍💻",
   description:
-    "Explore the world of web development with Asif Zulfiqar, a passionate Software Engineer skilled in React.js, Next.js, Tailwind CSS, and TypeScript, and experienced in building scalable backends with Node.js, Express, MongoDB, and PostgreSQL. Explore my work and experience in building dynamic, scalable web applications and crafting visually stunning, interactive web solutions.",
+    "Explore the world of web development with Asif Zulfiqar, a passionate Software Engineer skilled in React.js, Next.js, Tailwind CSS, and TypeScript, and experienced in building scalable backends with Node.js, Express, MongoDB, and PostgreSQL.",
 };
 
 export default function Home() {
@@ -21,14 +19,14 @@ export default function Home() {
     <>
       <Header />
       <main className="flex-1">
-        {/* SEO-Only Content (hidden from users, visible to Google and screen readers) */}
+        {/* SEO-visible content directly on the page */}
         <section className="sr-only">
-          <h1>Asif Zulfiqar - Software Engineer</h1>
+          <h1>Asif Zulfiqar - Software Engineer 👨🏻‍💻</h1>
           <p>
             Explore the world of web development with Asif Zulfiqar, a
             passionate Software Engineer skilled in React.js, Next.js, Tailwind
-            CSS, TypeScript, and building scalable backends with Node.js,
-            Express, MongoDB, and PostgreSQL.
+            CSS, TypeScript, and experienced in building scalable backends with
+            Node.js, Express, MongoDB, and PostgreSQL.
           </p>
         </section>
 
