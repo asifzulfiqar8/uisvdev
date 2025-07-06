261ccfa9b3f871f78502a7a49ace16e3cd8c5ad4
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-07-07 02:29:14 +0500

    fix: metadata added

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import { HeroSection } from "./components/sections/hero";
 import { ProjectsSection } from "./components/sections/projects";
 import { SkillsSection } from "./components/sections/skills";
 import ContactSection from "./components/sections/contact";
-import { Metadata } from "next";
 
+// This enables Static Site Generation
 export const dynamic = "force-static";
-export const revalidate = false;
+export const revalidate = false; // This ensures the page is fully static
 
-export const metadata: Metadata = {
+// Static metadata for better SEO
+export const metadata = {
   title: "Asif Zulfiqar - Software Engineer 👨🏻‍💻",
   description:
     "Explore the world of web development with Asif Zulfiqar, a passionate Software Engineer skilled in React.js, Next.js, Tailwind CSS, and TypeScript, and experienced in building scalable backends with Node.js, Express, MongoDB, and PostgreSQL.",
@@ -20,17 +21,6 @@ export default function Home() {
     <>
       <Header />
       <main className="flex-1">
-        {/* SEO Text for crawlers */}
-        <section className="sr-only">
-          <h1>Asif Zulfiqar - Software Engineer 👨🏻‍💻</h1>
-          <p>
-            Explore the world of web development with Asif Zulfiqar, a
-            passionate Software Engineer skilled in React.js, Next.js, Tailwind
-            CSS, TypeScript, and building scalable backends with Node.js,
-            Express, MongoDB, and PostgreSQL.
-          </p>
-        </section>
-
         <HeroSection />
         <SkillsSection />
         <ProjectsSection />
